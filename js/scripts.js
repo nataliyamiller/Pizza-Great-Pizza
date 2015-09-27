629e28b9b986af817cc7e396e3b784f73c975de5
nataliyamiller <nataliyamiller82@gmail.com>
2015-09-26 21:01:19 -0700

    Passes spec for adding customer toppings

--- js/scripts.js
+++ js/scripts.js
@@ -2,30 +2,16 @@ var pizzaSizes = ["Small", "Medium", "Large", "Extra Large", "Giant"];
 var pizzaToppings= ["Double Cheese", "Salami", "Smoked Bacon", "Pineapple", "Green Peppers", "Mashrooms"];
 
 function Pizza() {
-  this.sizes = pizzaSizes;
-  this.toppings = pizzaToppings;
+  this.availableSizes = pizzaSizes;
+  this.availableToppings = pizzaToppings;
+  this.chosenToppings = [];
   this.price = 0;
-  this.quantity = 0;
 }
 
-Pizza.prototype.getSizeIndex = function(chosenSize) {
-  var result;
-  chosenSize.toLowerCase();
-  for (var i = 0; i < this.sizes.length; i++) {
-    if (this.sizes[i] === chosenSize) {
-      result = i;
-    }
-  }
-  return result;
+Pizza.prototype.addToppings = function(chosenTopping) {
+  return this.chosenToppings.push(chosenTopping);
 }
 
-Pizza.prototype.getToppingIndex = function(chosenTopping) {
-  var result;
-  chosenTopping.toLowerCase();
-  for (var i = 0; i < this.toppings.length; i++) {
-    if (this.toppings[i] === chosenTopping) {
-      result = i;
-    }
-  }
-  return result;
+Pizza.prototype.priceForToppings = function() {
+  return this.toppingsPrice = this.chosenToppings.length * 0.5;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,16 +1,21 @@
 describe('Pizza', function() {
-  var testPizza = new Pizza();
   it("creates a new pizza with the given specifications", function() {
-    expect(testPizza.sizes.length).to.equal(5);
-    expect(testPizza.toppings).to.eql(["Double Cheese", "Salami", "Smoked Bacon", "Pineapple", "Green Peppers", "Mashrooms"]);
+    var testPizza = new Pizza();
+    expect(testPizza.availableSizes.length).to.equal(5);
+    expect(testPizza.availableToppings).to.eql(["Double Cheese", "Salami", "Smoked Bacon", "Pineapple", "Green Peppers", "Mashrooms"]);
     expect(testPizza.price).to.equal(0);
   });
 
-  it("finds an index of a given pizza size in the sizes array", function() {
-    expect(testPizza.getSizeIndex("Medium")).to.equal(1);
+  it("adds customer's chozen toppings to the array", function() {
+    var testPizza = new Pizza();
+    testPizza.addToppings("Double Cheese");
+    expect(testPizza.chosenToppings).to.eql(["Double Cheese"])
   });
 
-  it("finds an index of a given pizza topping in the toppings array", function() {
-    expect(testPizza.getToppingIndex("Salami")).to.equal(1);
+  it("calculates total price for the chosen toppings", function() {
+    var testPizza = new Pizza();
+    testPizza.addToppings("Double Cheese");
+    testPizza.addToppings("Salami");
+    expect(testPizza.priceForToppings()).to.equal(1);
   });
 });
