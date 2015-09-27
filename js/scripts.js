238a0fccd54a7b613308a2e9a8b1ba63384b12c1
nataliyamiller <nataliyamiller82@gmail.com>
2015-09-27 00:23:40 -0700

    Adds html file with some styling

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ pizzaSizes["Large"] = 14;
 pizzaSizes["Extra Large"] = 17;
 pizzaSizes["Giant"] = 23;
 
-var pizzaToppings= ["Double Cheese", "Salami", "Smoked Bacon", "Pineapple", "Green Peppers", "Mashrooms"];
+var pizzaToppings= ["Double Cheese", "Salami", "Smoked Bacon", "Pineapple", "Green Peppers", "Mashrooms", "Some Humor"];
 
 function Pizza(size) {
   this.availableSizes = pizzaSizes;
@@ -31,3 +31,34 @@ Pizza.prototype.totalPrice = function() {
    result += this.priceForSize(this.chosenSize) + this.priceForToppings();
    return result;
 }
+
+var appendSizeButtons = function() {
+  // var keys = Object.keys(pizzaSizes);
+  var append = "";
+  for (key in pizzaSizes) {
+    append += '<label class="sizes"><input type="radio" name="size" value="' + key + '"> ' + key + ' </input></label><br>';
+  }
+  return append;
+}
+
+var appendToppingsBoxes = function() {
+  var append = "";
+  pizzaToppings.forEach(function(topping) {
+    append += '<label class="toppings"><input type="checkbox" name="topping" value="'+ topping +'"> '+ topping + ' </label><br>';
+  });
+  return append;
+}
+
+
+$(document).ready(function() {
+  $("p").append(appendSizeButtons());
+  $("h2").append(appendToppingsBoxes());
+  $(":radio").wrap("<span style='background-color:red'>");
+  $(":checkbox").wrap("<span style='background-color:blue'>");
+});
+//   <form>
+//   <input type="radio" name="sex" value="male" checked> Male
+//   <br>
+//   <input type="radio" name="sex" value="female"> Female
+// </form>
+// }
