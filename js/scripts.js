2330c0155f93790a7bc6d0bdce956543c62ea138
nataliyamiller <nataliyamiller82@gmail.com>
2015-09-27 21:01:30 -0700

    Passes spec for customer order

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,12 @@ pizzaSizes["Large"] = 14;
 pizzaSizes["Extra Large"] = 17;
 pizzaSizes["Giant"] = 23;
 
-var pizzaToppings= ["Double Cheese", "Salami", "Smoked Bacon", "Pineapple", "Green Peppers", "Mashrooms", "Some Humor"];
+var pizzaToppings = ["Double Cheese", "Salami", "Smoked Bacon", "Pineapple", "Green Peppers", "Mashrooms", "Some Humor"];
 
-function Pizza(size) {
+function Pizza() {
   this.availableSizes = pizzaSizes;
   this.availableToppings = pizzaToppings;
-  this.chosenSize = size;
+  this.chosenSize = "";
   this.chosenToppings = [];
 }
 
@@ -18,25 +18,40 @@ Pizza.prototype.addToppings = function(chosenTopping) {
   return this.chosenToppings.push(chosenTopping);
 }
 
+Pizza.prototype.removeToppings = function(chosenTopping) {
+  var index = this.chosenToppings.indexOf(chosenTopping);
+  return this.chosenToppings.splice(index, 1);
+}
+
 Pizza.prototype.priceForToppings = function() {
   return this.toppingsPrice = this.chosenToppings.length * 0.5;
 }
 
+Pizza.prototype.chooseSize = function(size) {
+  return this.chosenSize = size;
+}
+
 Pizza.prototype.priceForSize = function(size) {
-  return _.propertyOf(pizzaSizes)(size);
+  return _.propertyOf(this.availableSizes)(size);
 }
 
 Pizza.prototype.totalPrice = function() {
-  var result = 0
-   result += this.priceForSize(this.chosenSize) + this.priceForToppings();
-   return result;
+   return this.priceForSize(this.chosenSize) + this.priceForToppings();
 }
 
-var appendSizeButtons = function() {
-  // var keys = Object.keys(pizzaSizes);
+function CustomerOrder(pizza) {
+  this.pizza = pizza;
+  this.quantity = 0;
+}
+
+CustomerOrder.prototype.totalOrder = function() {
+  return this.pizza.totalPrice() * this.quantity;
+}
+
+Pizza.prototype.appendSizeButtons = function() {
   var append = "";
-  for (key in pizzaSizes) {
-    append += '<label class="sizes"><input type="radio" name="size" value="' + key + '"> ' + key + ' </input></label><br>';
+  for (key in this.availableSizes) {
+    append += '<label class="sizes"><input type="radio" name="size" value="' + key + '"> ' + key + ' </label><br>';
   }
   return append;
 }
@@ -44,21 +59,58 @@ var appendSizeButtons = function() {
 var appendToppingsBoxes = function() {
   var append = "";
   pizzaToppings.forEach(function(topping) {
-    append += '<label class="toppings"><input type="checkbox" name="topping" value="'+ topping +'"> '+ topping + ' </label><br>';
+    append += '<label class="toppings"><input type="checkbox" name="checkme" value="'+ topping +'"> '+ topping + ' </label><br>';
   });
   return append;
 }
 
+var appendQuantityBox = function() {
+return  '<label for="quantity">Quantity  </label>' +
+        '<input type="number" name="quantity" class="quantity" value="0" min="1" max="10">';
+}
+
 
 $(document).ready(function() {
-  $("p").append(appendSizeButtons());
+  $(".result").hide();
   $("h2").append(appendToppingsBoxes());
-  $(":radio").wrap("<span style='background-color:red'>");
+  $("#quantity-box").append(appendQuantityBox());
   $(":checkbox").wrap("<span style='background-color:blue'>");
+  var newPizza = new Pizza();
+  $("p").append(newPizza.appendSizeButtons());
+  var newOrder = new CustomerOrder(newPizza);
+
+  $(":radio").change(function() {
+    var size = $(this).val();
+    newPizza.chooseSize(size);
+    newPizza.priceForSize(size);
+    $("#selected-size").text(size);
+    $("#size-price").text("$" + newPizza.priceForSize(size));
+  });
+
+
+  $(".quantity").change(function() {
+    newOrder.quantity = $("input[name=quantity]").val();
+  });
+
+  $(":checkbox").click(function() {
+    var topping = $(this).val();
+    if ($(this).is(':checked')) {
+      if(newPizza.chosenToppings.indexOf(topping) < 0) {
+        newPizza.addToppings(topping);
+      }
+    } else {
+    newPizza.removeToppings(topping);
+    }
+    newPizza.chosenToppings;
+    newPizza.priceForToppings();
+  $("#selected-toppings").text(newPizza.chosenToppings);
+  $("#toppings-price").text("$" + newPizza.priceForToppings());
+  });
+
+  $("#order").click(function() {
+
+    $(".result").show();
+    var price = newOrder.totalOrder();
+  $("#total-price").text("$" + price);
 });
-//   <form>
-//   <input type="radio" name="sex" value="male" checked> Male
-//   <br>
-//   <input type="radio" name="sex" value="female"> Female
-// </form>
-// }
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,9 @@
 describe('Pizza', function() {
   it("creates a new pizza with the given specifications", function() {
-    var testPizza = new Pizza("Medium");
+    var testPizza = new Pizza();
     var availableSizesObject = testPizza.availableSizes;
     expect(Object.keys(availableSizesObject)).to.eql(["Small", "Medium", "Large", "Extra Large", "Giant"]);
-    expect(testPizza.availableToppings).to.eql(["Double Cheese", "Salami", "Smoked Bacon", "Pineapple", "Green Peppers", "Mashrooms"]);
+    expect(testPizza.availableToppings).to.eql(["Double Cheese", "Salami", "Smoked Bacon", "Pineapple", "Green Peppers", "Mashrooms", "Some Humor"]);
   });
 
   it("adds customer's chozen toppings to the array", function() {
@@ -25,9 +25,22 @@ describe('Pizza', function() {
   });
 
   it("returns total price for pizza with selected toppings and size", function() {
-    var testPizza = new Pizza("Large");
+    var testPizza = new Pizza();
+    testPizza.chooseSize("Medium");
     testPizza.addToppings("Double Cheese");
     testPizza.addToppings("Salami");
-    expect(testPizza.totalPrice()).to.equal(15);
+    expect(testPizza.totalPrice()).to.equal(10);
+  });
+});
+
+describe('CustomerOrder', function() {
+  it("creates a new customer order with the proviced pizza attributes", function() {
+    var pizza = new Pizza();
+    pizza.addToppings("Double Cheese");
+    pizza.chooseSize("Medium");
+    var order = new CustomerOrder(pizza);
+    expect(order.pizza).to.equal(pizza);
+    expect(order.pizza.chosenToppings).to.eql(["Double Cheese"]);
+    expect(order.pizza.chosenSize).to.equal("Medium");
   });
 });
